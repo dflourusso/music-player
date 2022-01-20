0c04a9ad293643b5aac15f2b36e125f4da634ea0
Daniel Fernando Lourusso <daniel.lourusso@db1.com.br>
2022-01-20 12:07:17 -0300

    feat: keep app awake

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,3 +1,4 @@
+import { useKeepAwake } from 'expo-keep-awake'
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { Text } from 'react-native'
@@ -6,6 +7,7 @@ import Soundlist from './SoundList'
 import { Container } from './styles'
 
 export default function App() {
+  useKeepAwake()
   return (
     <Container>
       <StatusBar style='light' />
